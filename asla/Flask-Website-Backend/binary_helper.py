176cda9a21471e7ed3ec64bcf2feed1d1b4e8fe7
Srinivas <srinivas.suresh.kumar@gmail.com>
2016-10-17 18:31:51 -0400

    Added stubs for the web server

---
+++ asla/Flask-Website-Backend/binary_helper.py
@@ -0,0 +1,16 @@
+class BinaryService:
+    """
+    Class is responsible for fetching the right binaries on request
+    """
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def fetch_binary(type):
+        """
+        Fetches the right binary for the request
+        :param type: the type of binary to fetch User or Expert
+        :return:
+        """
+        return None
---
+++ asla/Flask-Website-Backend/user_admin_service.py
@@ -0,0 +1,23 @@
+class UserAdminService:
+    """
+    Class that handles all tasks related to user administration
+    """
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def make_new_user():
+        """
+        Makes a new user and puts him in the database
+        :return:
+        """
+        return None
+
+    @staticmethod
+    def authenticate_user():
+        """
+        Authenticates a user or login
+        :return:
+        """
+        return None
