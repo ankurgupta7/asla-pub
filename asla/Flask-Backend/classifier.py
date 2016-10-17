d6a8c6428f6f784dfe895e34b0d9efcf67f74f45
tbonjour <bonjour.trevor@gmail.com>
2016-10-17 18:06:36 -0400

    added __init__

--- asla/Flask-Backend/classifier.py
+++ asla/Flask-Backend/classifier.py
@@ -1,3 +1,5 @@
+
+
 class Classifier:
     """
     The classifier class that will invoke the classifier on some data
--- asla/Flask-Backend/global_model_service.py
+++ asla/Flask-Backend/global_model_service.py
@@ -7,6 +7,9 @@ class GlobalModelService:
     Service that performs global model maintenance
     """
 
+    def __init__(self):
+        pass
+
     @staticmethod
     def generate_model(data):
         """
