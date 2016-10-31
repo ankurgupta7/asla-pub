31e1f91bffda854b17ea800896f4d268e5fcb9cf
Ankur Gupta <ankurgupta7@gmail.com>
2016-10-30 23:44:01 -0400

    adding sip and qstring for cross platform compatibility

--- asla/binary/user_ui/main.py
+++ asla/binary/user_ui/main.py
@@ -5,22 +5,22 @@ from main_window import MainWindow
 import sip
 import sys
 
-sip.setapi("QString", 2)
+sip.setapi("QString", 1)
 
 
 if __name__ == "__main__":
     a = QApplication(sys.argv)
 
-    loginDialog = LoginDialog()
+    # loginDialog = LoginDialog()
 
-    isAuth = False
-    result = -1
-    while not isAuth:
-        result = loginDialog.exec_()
-        if result == LoginDialog.Success or result == LoginDialog.Rejected:
-            isAuth = True
-        else:
-            isAuth = False
+    # isAuth = False
+    # result = -1
+    # while not isAuth:
+    #     result = loginDialog.exec_()
+    #     if result == LoginDialog.Success or result == LoginDialog.Rejected:
+    #         isAuth = True
+    #     else:
+    #         isAuth = False
 
 
     # if result == LoginDialog.Success:
--- asla/binary/user_ui/main_window.py
+++ asla/binary/user_ui/main_window.py
@@ -2,16 +2,29 @@
 from ui_main_window import Ui_MainWindow
 from PyQt4 import QtGui, QtCore
 # from asla..predict_service import PredictService
-
+from PyQt4.QtGui import QApplication
 from binary.ml_tools.predict_service import PredictService
 
 import threading
 import time
 
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QApplication.translate(context, text, disambig)
 
 class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
     def __init__(self):
-        QtGui.QMainWindow.__init__()
+        QtGui.QMainWindow.__init__(self)
         self.setupUi(self)
 
         self.predict_service = PredictService('','')
