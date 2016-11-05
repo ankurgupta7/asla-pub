ef317d56000b88f6197d0f30c039023fb0630193
tbonjour <bonjour.trevor@gmail.com>
2016-11-05 16:18:01 -0400

    removed CRUX

--- ml_analysis/gesture_collection.py
+++ ml_analysis/gesture_collection.py
@@ -13,17 +13,8 @@ if platform == "linux" or platform == "linux2":
         from lib.x64 import Leap
     else:
         from lib.x86 import Leap
-    # src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
-    # arch_dir = './lib/x64' if sys.maxsize > 2 ** 32 else './lib/x86'
-    # sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
 elif platform == "darwin":
     from lib import Leap
-    # src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
-    # lib_dir = os.path.abspath(os.path.join(src_dir, './lib'))
-    # sys.path.insert(0, lib_dir)
-
-# import Leap
-
 
 class GestureCollection:
     """
