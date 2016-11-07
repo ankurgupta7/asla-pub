f99c4acd401881bc41fcc12d479abb59efc6829c
Srinivas <srinivas.suresh.kumar@gmail.com>
2016-11-07 14:01:18 -0500

    drax, them, sklounz

--- asla/server/flask_website/FlaskWebsiteBackend.py
+++ asla/server/flask_website/FlaskWebsiteBackend.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 
@@ -65,4 +66,5 @@ def authenticate():
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
