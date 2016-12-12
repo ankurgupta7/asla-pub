cff9f6ffef2bc1a48a893789fb845534a1a2ccfa
atah1991 <atahodjaev@gmail.com>
2016-12-12 00:18:53 -0500

    implemented send_to_server method

--- asla/binary/ml_tools/training_service.py
+++ asla/binary/ml_tools/training_service.py
@@ -2,7 +2,8 @@ import os, sys
 root_path = os.path.dirname(os.path.abspath('..'))
 sys.path.append(root_path)
 from ..leap_tools.gesture_collection import GestureCollection
-
+from pymongo import MongoClient
+import csv
 
 class TrainingService:
     """
@@ -13,6 +14,9 @@ class TrainingService:
     def __init__(self):
         # this is the final data set that is sent to the server
         # for now, this will be stored locally in a csv file
+        self.mongo_client = MongoClient("mongodb://asla-expert:asla@ds149207.mlab.com:49207/trainingdata")
+        self.db = self.mongo_client['trainingdata']
+        self.model_data = self.db['globalmodeldata']
         self.data_collected = []
         pass
 
@@ -30,9 +34,16 @@ class TrainingService:
         self.data_collected.extend(self.exp_ges.extract_features())
 
     def send_to_server(self):
-        """
-        Sends the data to Controller(Server)
-        """
-        return None
+        #data_to_send = {"training_data" : self.data_collected}
+        header_string = open('headers.csv')
+        column_labels = header_string.read().split(',')
+        data = open('data-20161208-131001_damian.csv')
+        lines = data.readlines()[1:]
+        for line in lines:
+            data_to_send = {}
+            values_per_line = line.split(',')
+            for j in column_labels:
+                data_to_send[j] = values_per_line[j]
+            self.model_data.insert_one(data_to_send)
     def set_status_bar(self, s):
         self.exp_ges.setStatusBar(s)
