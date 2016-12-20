cc53fc0eed59ebd2bea46c42ee4b332a1044ba84
tbonjour <bonjour.trevor@gmail.com>
2016-12-20 05:37:14 -0500

    added classifiers

--- asla/server/binary_server_backend/classifier.py
+++ asla/server/binary_server_backend/classifier.py
@@ -4,6 +4,8 @@ from abc import abstractmethod
 from sklearn import svm
 from sklearn import preprocessing
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 
 class Classifier:
@@ -20,7 +22,6 @@ class Classifier:
     @abstractmethod
     def create_model(self, x, y):
         """
-
         :param x: Features of data to be trained on
         :type x: 2D numpy array, 2d list, or a data frame
         :param y: Labels of the corresponding features
@@ -45,12 +46,36 @@ class SVM(Classifier):
 
 
 class KNN(Classifier):
-    """Ball Tree K-Nearest Neighbour Classifier"""
+    """k-Nearest Neighbours Classifier"""
     def __init__(self):
         Classifier.__init__(self)
 
     def create_model(self, x, y):
         Classifier.create_model(x, y)
-        knc = KNeighborsClassifier(n_neighbors=5, algorithm='ball_tree')
+        knc = KNeighborsClassifier(10)
         self.fin_model = knc.fit(self.x_train_scaled, y)
+        return self.fin_model, self.scaler
+
+
+class RFC(Classifier):
+    """Random forest Classifier"""
+    def __init__(self):
+        Classifier.__init__(self)
+
+    def create_model(self, x, y):
+        Classifier.create_model(x, y)
+        rfc = RandomForestClassifier(max_depth=800, n_estimators=800, max_features='auto', n_jobs=8)
+        self.fin_model = rfc.fit(self.x_train_scaled, y)
+        return self.fin_model, self.scaler
+
+
+class DTC(Classifier):
+    """Decision Tree Classifier"""
+    def __init__(self):
+        Classifier.__init__(self)
+
+    def create_model(self, x, y):
+        Classifier.create_model(x, y)
+        dtc = DecisionTreeClassifier(max_depth=3000)
+        self.fin_model = dtc.fit(self.x_train_scaled, y)
         return self.fin_model, self.scaler
