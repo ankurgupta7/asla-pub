2f7069d98b52fc60b6052a2deabfda4ce00d1fa8
tbonjour <bonjour.trevor@gmail.com>
2016-12-21 22:12:50 -0500

    added functionality for socres_per_letter and confusion matrix

--- ml_analysis/analysis.py
+++ ml_analysis/analysis.py
@@ -6,7 +6,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import LeaveOneGroupOut
 from sklearn import preprocessing, metrics
 import matplotlib.pyplot as plt
-
+import string
 
 import glob
 # This script is used for comparing the performance of various classifiers.
@@ -20,7 +20,6 @@ classifiers = [
     SVC(kernel="linear", C=0.04),
     DecisionTreeClassifier(max_depth=3000),
     RandomForestClassifier(max_depth=800, n_estimators=800, max_features='auto', n_jobs=8)]
-
 
 user_data = {}
 users = []
@@ -54,17 +53,37 @@ accuracy = {}
 
 for name, clf in zip(names, classifiers):
     scores = []
+    score_per_label = []
     print '###########################################################################'
     print 'Classifier: ', name
-
+    confusion_mat = np.zeros(shape=(26, 26))
     for train, test in logo.split(x_train_scaled, y_train, groups=groups):
         clf.fit(x_train_scaled[train], y_train[train])
         scores.append(clf.score(x_train_scaled[test], y_train[test]))
         y_predicted = clf.predict(x_train_scaled[test])
-        # print metrics.classification_report(y_train[test], y_predicted)
+        score_per_label.append(metrics.f1_score(y_train[test], y_predicted, average=None))
+        confusion_mat = confusion_mat + metrics.confusion_matrix(y_train[test], y_predicted)
     print scores
     print("CV Accuracy: %0.2f (+/- %0.2f)" % (np.mean(scores), np.std(scores) * 2))
     accuracy[name] = [np.mean(scores), np.std(scores) * 2]
+    print np.shape(np.mean(np.array(score_per_label), axis=0))
+    print
+    print 'SCORES PER LABEL \n'
+    print '| Label         | Accuracy      |'
+    print '|:-------------:|:-------------:|'
+    for label, acc in zip(list(string.ascii_uppercase), np.mean(np.array(score_per_label), axis=0)):
+        print '|', label, '|', '%0.2f ' % acc, '|'
+    print '\n'
+    print 'CONFUSION MATRIX\n'
+    for i, acc in enumerate(np.mean(np.array(score_per_label), axis=0)):
+        if acc < 0.61:
+            raw = confusion_mat[i]
+            norm = [float(j) / sum(raw) for j in raw]
+            c = []
+            for alpha, conf in zip(list(string.ascii_uppercase), norm):
+                if conf > 0.05:
+                    c.append((alpha, conf))
+            print '|', list(string.ascii_uppercase)[i], '|', c, '|'
 
 bar_width = 0.2
 opacity = 0.6
@@ -81,7 +100,7 @@ for key, value in accuracy.items():
                      label=key)
     ax.text(index + bar_width, value[0]+value[1] + 0.02, "%0.2f" % (value[0]), color='black', fontweight='bold')
     index += 1
-    print key, value
+    # print key, value
 
 ax.set_aspect('auto')
 index = np.arange(4)
